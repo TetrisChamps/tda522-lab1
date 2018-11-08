2ab674447e857607680f9a23d1cd6ae5397b985c
David Olsson <d.e.d.davidolsson@gmail.com>
2018-11-08 14:15:50 +0100

    Upg A

---
+++ src/Car.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+
+public abstract class Car {
+
+
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power of the car
+    private double currentSpeed; // The current speed of the car
+    private Color color; // Color of the car
+    private final String modelName; // The car model name
+
+    public Car(int nrDoors, double enginePower, Color color, String modelName) {
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.color = color;
+        this.modelName = modelName;
+        stopEngine();
+    }
+
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    private void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    public abstract double speedFactor();
+
+    private void incrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+    }
+
+    private void decrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+    }
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
+}
